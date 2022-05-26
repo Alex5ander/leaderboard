8208a1a4e1b0adfae14312b79a19e444341d5edc
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2022-05-26 13:11:55 -0300

    ajuste

--- src/main/java/com/leaderboard/leaderboard/LeaderboardApplication.java
+++ src/main/java/com/leaderboard/leaderboard/LeaderboardApplication.java
@@ -1,15 +1,23 @@
 package com.leaderboard.leaderboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @SpringBootApplication
-public class LeaderboardApplication {
+public class LeaderboardApplication implements CommandLineRunner {
 
     @Autowired
     static ScoreRepository ScoreItem;
     public static void main(String[] args) {
         SpringApplication.run(LeaderboardApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        ScoreItem.findAll();
     }
 }
--- src/main/java/com/leaderboard/leaderboard/Score.java
+++ src/main/java/com/leaderboard/leaderboard/Score.java
@@ -1,9 +1,10 @@
 package com.leaderboard.leaderboard;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
-
+@Document("score")
 public class Score {
     @Id
     private final int id;
